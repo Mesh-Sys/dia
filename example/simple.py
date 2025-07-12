74c44a1b2250ab1ea264504e098b346c6726e5e9
SkyNotion <meshdrew5@gmail.com>
2025-07-12 03:39:46 +0100

    Change VC prompt

--- example/simple.py
+++ example/simple.py
@@ -4,7 +4,7 @@ from dia.model import Dia
 model = Dia.from_pretrained("nari-labs/Dia-1.6B-0626", compute_dtype="float16")
 
 #text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on Git hub or Hugging Face."
-text = "[S1] Huge thanks to Asmongold for somehow finding my video and then reacting to it, and to everyone who's here now because of that. I'm legitimately like brand new to YouTube, and I genuinely did not expect this video to get really any attention, much less blow up now the way that it has, but here we are. So before all this, it really was just me and my 300 Spartans holding the line in the comments section and now it appears we've been overrun by roaches, which by the way, you guys [S2] Thanks to you all"
+text = "[S1] Huge thanks to Asmongold for somehow finding my video and then reacting to it, and to everyone who's here now because of that. I'm legitimately like brand new to YouTube, and I genuinely did not expect this video to get really any attention, much less blow up now the way that it has, but here we are. So before all this, it really was just me and my 300 Spartans holding the line in the comments section and now it appears we've been overrun by roaches, which by the way, you guys."
 
 output = model.generate(
     text,
