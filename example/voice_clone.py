7cb5e6457f75a7d321db12e5fde9c6bbeabdf456
SkyNotion <meshdrew5@gmail.com>
2025-07-12 02:51:08 +0100

    Change simple prompt

--- example/voice_clone.py
+++ example/voice_clone.py
@@ -7,7 +7,7 @@ model = Dia.from_pretrained("nari-labs/Dia-1.6B-0626", compute_dtype="float16")
 # We will use the audio created by running simple.py as an example.
 # Note that you will be REQUIRED TO RUN simple.py for the script to work as-is.
 #clone_from_text = "[S1] Dia is an open weights text to dialogue model. [S2] You get full control over scripts and voices. [S1] Wow. Amazing. (laughs) [S2] Try it now on Git hub or Hugging Face."
-clone_from_text = "[S1] Huge thanks to Asmongold for somehow finding my video and then reacting to it, and to everyone who's here now because of that. I'm legitimately like brand new to YouTube, and I genuinely did not expect this video to get really any attention, much less blow up now the way that it has, but here we are. So before all this, it really was just me and my 300 Spartans holding the line in the comments section and now it appears we've been overrun by roaches, which by the way, you guys,"
+clone_from_text = "[S1] Huge thanks to Asmongold for somehow finding my video and then reacting to it, and to everyone who's here now because of that. I'm legitimately like brand new to YouTube, and I genuinely did not expect this video to get really any attention, much less blow up now the way that it has, but here we are. So before all this, it really was just me and my 300 Spartans holding the line in the comments section and now it appears we've been overrun by roaches, which by the way, you guys [S2] Thanks to you all"
 #clone_from_audio = "simple.mp3"
 clone_from_audio = "girl.wav"
 
